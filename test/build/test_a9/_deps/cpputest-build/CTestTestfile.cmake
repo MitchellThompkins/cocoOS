e996d1b1c646bd765398fb7596b97e9ba0e95feb
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-07-29 21:51:09 -0700

    moving stuff around

--- test/build/test_a9/_deps/cpputest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mthompkins/workspace/cocoOS/test/build/test_a9/_deps/cpputest-src
-# Build directory: /home/mthompkins/workspace/cocoOS/test/build/test_a9/_deps/cpputest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- test/build/test_a9/_deps/cpputest-build/src/CppUTestExt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mthompkins/workspace/cocoOS/test/build/test_a9/_deps/cpputest-src/src/CppUTestExt
-# Build directory: /home/mthompkins/workspace/cocoOS/test/build/test_a9/_deps/cpputest-build/src/CppUTestExt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
