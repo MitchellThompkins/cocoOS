430f4b6ae3bf1a1574116cd6370ff12335a290e1
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-08-03 09:18:38 -0700

    okay, this did build

---
+++ os/os_utils/mocks/src/mock_os_assert.c
@@ -0,0 +1,10 @@
+#include "CppUTestExt/MockSupport_c.h"
+
+#include "stdio.h"
+#include "os_assert.h"
+
+void os_on_assert(const char* file, uint16_t line, const char* expr)
+{
+    printf("called mock");
+    mock_c()->actualCall("os_assert");
+}
--- tests/mocks/include/mocks.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MOCKS_HPP
-#define MOCKS_HPP
-
-void os_assert();
-
-#endif
