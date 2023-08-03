3bd702a213a37499fcd6e368a7ece0db2b910211
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-08-02 23:14:51 -0700

    removed example, previous one worked

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -11,29 +11,12 @@
 #include "os_task.h"
 #include "os_kernel.h"
 
-static void hello_task(void)
-{
-    task_open();
-
-    for(;;)
-    {
-        printf("hello world\n");
-        task_wait( 20 );
-    }
-
-    task_close();
-}
-
 int main(int argc, char *argv[])
 {
     const uint16_t interval_ms = 250;
 
     platform_setup_timer(interval_ms);
     platform_enable_timer();
-
-    os_init();
-    task_create( hello_task, NULL, 1, NULL, 0, 0 );
-    os_start();
 
     int result = CommandLineTestRunner::RunAllTests(argc, argv);
 
