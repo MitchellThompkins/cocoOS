fb5bc1a836d15331c76487399c66142055876b88
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-08-07 23:18:01 -0700

    this fails strangely

--- tests/os_task/src/os_task.test.cpp
+++ tests/os_task/src/os_task.test.cpp
@@ -100,3 +100,20 @@ TEST(TestOsTask, fail_create_task_when_os_is_running)
     mock().clear();
 
 }
+
+// TASK-3
+TEST(TestOsTask, successful_task_initialization)
+{
+    mock().expectOneCall("os_init");
+    os_init();
+
+    // Verify that you cannot create a task while os is running
+    const auto id1 {task_create( dummy_task, NULL, 1, NULL, 0, 0 )};
+    CHECK_EQUAL(0, id1);
+
+    const auto id2 {task_create( dummy_task, NULL, 2, NULL, 0, 0 )};
+    CHECK_EQUAL(1, id2);
+
+    const auto id3 {task_create( dummy_task, NULL, 3, NULL, 0, 0 )};
+    CHECK_EQUAL(2, id3);
+}
