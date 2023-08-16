6477d2d1d878d25c312fb6df054db09fa085b5e9
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-08-15 21:02:45 -0700

    x86_64 builds now

--- os/os_utils/include/os_defines.h
+++ os/os_utils/include/os_defines.h
@@ -3,60 +3,59 @@
 
 #include "user_os_config.h"
 
-/** @file os_defines.h cocoOS user configuration
-          Macros can be defined in this file or as compiler flags e.g. -DN_TASKS=2 -DN_EVENTS=3...
-*/
-
+#ifndef N_TASKS
+#error N_TASKS must be defined with #define
+#endif
 #ifdef __cplusplus
-extern "C" {
+static_assert(N_TASKS>=0 && N_TASKS<255, "Maximum number of tasks is 254");
+#else
+_Static_assert(N_TASKS>=0 && N_TASKS<255, "Maximum number of tasks is 254");
 #endif
 
-/** Max number of used tasks
-* @remarks Must be defined. @n Allowed range: 0-254. Value must not be exceeded */
-//#ifndef N_TASKS
-//#define N_TASKS 1
-//#endif
-_Static_assert (N_TASKS>=0 && N_TASKS<255, "Maximum number of tasks is 254");
+
+#ifndef N_QUEUES
+#error N_QUEUES must be defined with #define
+#endif
+#ifdef __cplusplus
+static_assert(N_QUEUES >= 0 && N_QUEUES < 255, "Maximum number of queues is 254");
+#else
+_Static_assert(N_QUEUES >= 0 && N_QUEUES < 255, "Maximum number of queues is 254");
+#endif
 
 
-
-/** Max number of used message queues
-* @remarks Must be defined. @n Allowed range: 0-254. Value must not be exceeded */
-//#ifndef N_QUEUES
-//#define N_QUEUES 0
-//#endif
-_Static_assert (N_QUEUES >= 0 && N_QUEUES < 255, "Maximum number of queues is 254");
-
-
-/** Max number of used semaphores
-* @remarks Must be defined. @n Allowed range: 0-254. Value must not be exceeded */
-//#ifndef N_SEMAPHORES
-//#define N_SEMAPHORES 0
-//#endif
-_Static_assert (N_SEMAPHORES >=0 && N_SEMAPHORES < 255, "Maximum number of queues is 254");
+#ifndef N_SEMAPHORES
+#error N_SEMAPHORES must be defined with #define
+#endif
+#ifdef __cplusplus
+static_assert(N_SEMAPHORES >=0 && N_SEMAPHORES < 255, "Maximum number of queues is 254");
+#else
+_Static_assert(N_SEMAPHORES >=0 && N_SEMAPHORES < 255, "Maximum number of queues is 254");
+#endif
 
 
-/** Max number of used events
-* @remarks Must be defined. @n Allowed range: 0-254. Value must not be exceeded */
-//#ifndef N_EVENTS
-//#define N_EVENTS 0
-//#endif
-_Static_assert (N_EVENTS >=0 && N_EVENTS < 255, "Maximum number of events is 254");
+#ifndef N_QUEUES
+#error N_EVENTS must be defined with #define
+#endif
+#ifdef __cplusplus
+static_assert(N_EVENTS >=0 && N_EVENTS < 255, "Maximum number of events is 254");
+#else
+_Static_assert(N_EVENTS >=0 && N_EVENTS < 255, "Maximum number of events is 254");
+#endif
 
 
-/** Round Robin scheduling
-* @remarks If defined, tasks will be scheduled ignoring the priorities */
-//#ifndef ROUND_ROBIN
-//#define ROUND_ROBIN 0
-//#endif
-_Static_assert (ROUND_ROBIN == 0 || ROUND_ROBIN == 1, "ROUND_ROBIN must be 0 or 1");
+#ifndef N_QUEUES
+#error ROUND_ROBIN must be defined as 0 or 1
+#endif
+#ifdef __cplusplus
+static_assert(ROUND_ROBIN == 0 || ROUND_ROBIN == 1, "ROUND_ROBIN must be 0 or 1");
+#else
+_Static_assert(ROUND_ROBIN == 0 || ROUND_ROBIN == 1, "ROUND_ROBIN must be 0 or 1");
+#endif
 
 
-/** Memory size
- * @remarks Should be set to the size of address pointer */
-//#ifndef Mem_t
-//#define Mem_t uint32_t
-//#endif
+#ifndef Mem_t
+#error A type (like uint32_t for example) must be defined for Mem_t
+#endif
 
 #define NO_MSG_ID   0xff
 #define ISR_TID     0xfe
@@ -67,9 +66,5 @@ _Static_assert (ROUND_ROBIN == 0 || ROUND_ROBIN == 1, "ROUND_ROBIN must be 0 or 
 
 /* Total number of events needed */
 #define N_TOTAL_EVENTS        ( N_EVENTS + N_QUEUES )
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
