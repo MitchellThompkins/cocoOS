5b6c378a8ddf44e78e43b8ba5961c75595a88ff6
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-08-16 16:21:05 -0700

    make this handle path'd arguments

--- debug.mk
+++ debug.mk
@@ -1,5 +1,20 @@
-.PHONY: qemu-debug.%
-qemu-debug.%:
+.PHONY: %.qemu-run
+%.qemu-run:
+	qemu-system-arm \
+		-nographic \
+		--no-reboot \
+		-serial stdio \
+		-monitor none \
+		-machine xilinx-zynq-a9 \
+		-cpu cortex-a9 \
+		-m 12M \
+		--semihosting \
+		-semihosting-config enable=on,target=native \
+		-kernel build/$*
+
+# For example: make a9/test_os_task0.elf.qemu-gdb
+.PHONY: %.qemu-gdb
+%.qemu-gdb:
 	qemu-system-arm \
 		-nographic \
 		--no-reboot \
@@ -11,23 +26,9 @@ qemu-debug.%:
 		-d guest_errors\
 		--semihosting \
 		-semihosting-config enable=on,target=native \
-		-kernel build/$*/test_os_task0.elf
+		-kernel build/$*
 
-.PHONY: qemu-run.%
-qemu-run.%:
-	qemu-system-arm \
-		-nographic \
-		--no-reboot \
-		-serial stdio \
-		-monitor none \
-		-machine xilinx-zynq-a9 \
-		-cpu cortex-a9 \
-		-m 12M \
-		--semihosting \
-		-semihosting-config enable=on,target=native \
-		-kernel build/$*/test_os_task0.elf
-
-.PHONY: gdb-debug.%
-gdb-debug.%:
-	gdb build/$*/test_os_task0.elf -ix .gdbinit
+.PHONY: %.gdb
+%.gdb:
+	gdb build/$* -ix .gdbinit
 
