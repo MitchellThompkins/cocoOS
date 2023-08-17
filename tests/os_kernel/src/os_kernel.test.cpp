067488e2477082be302ff5ccc46104d0aa101421
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-08-16 23:12:50 -0700

    adding more tests

--- tests/os_kernel/src/os_kernel.test.cpp
+++ tests/os_kernel/src/os_kernel.test.cpp
@@ -8,19 +8,41 @@
 
 #include "os_assert.h"
 #include "os_kernel.h"
+#include "os_task.h"
+
+static void dummy_task(void)
+{
+    task_open();
+
+    for(;;)
+    {
+        task_wait( 20 );
+    }
+
+    task_close();
+}
 
 TEST_GROUP(TestOsKernel)
 {
     void setup()
     {
+        os_task_init();
     }
 
     void teardown()
     {
+        mock().clear();
     }
 };
 
-TEST(TestOsKernel, temp)
+TEST(TestOsKernel, verify_schedule)
 {
+    mock().expectOneCall("os_init");
+    os_init();
+
+    const auto id0 {task_create( dummy_task, NULL, 3, NULL, 0, 0 )};
+    const auto id1 {task_create( dummy_task, NULL, 2, NULL, 0, 0 )};
+    const auto id2 {task_create( dummy_task, NULL, 1, NULL, 0, 0 )};
+
     CHECK_EQUAL(1,1);
 }
