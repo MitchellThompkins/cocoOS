a28b43f201495a4a4bdde65df4284b03b157253b
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-08-24 12:15:19 -0700

    2 dirs up?

--- os/cocoos/src/cocoos_public.c
+++ os/cocoos/src/cocoos_public.c
@@ -2,10 +2,10 @@
 // and this provides an easy way for them to include a single source file to
 // specify with their build system
 
-#include "../os_event/src/os_event.c"
-#include "../os_kernel/src/os_cbk.c"
-#include "../os_kernel/src/os_kernel.c"
-#include "../os_msgqueue/src/os_msgqueue.c"
-#include "../os_sem/src/os_sem.c"
-#include "../os_task/src/os_task.c"
-#include "../os_utils/src/os_assert.c"
+#include "../../os_event/src/os_event.c"
+#include "../../os_kernel/src/os_cbk.c"
+#include "../../os_kernel/src/os_kernel.c"
+#include "../../os_msgqueue/src/os_msgqueue.c"
+#include "../../os_sem/src/os_sem.c"
+#include "../../os_task/src/os_task.c"
+#include "../../os_utils/src/os_assert.c"
